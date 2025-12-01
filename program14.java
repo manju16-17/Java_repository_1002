4be1dfac9fab7ff8e025e51c7bd2f9638ad6aac3
manju16-17 <manju7654332@gmail.com>
2025-12-01 18:18:36 +0530

    Add Student class with constructors

---
+++ program14.java
@@ -0,0 +1,14 @@
+class Student {
+    String name;
+    int age;
+
+    Student() {   // No-arg constructor
+        name = "Unknown";
+        age = 0;
+    }
+
+    Student(String n, int a) { // Parameterized
+        name = n;
+        age = a;
+    }
+}
