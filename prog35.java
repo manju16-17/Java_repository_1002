59c9ef407acd5a8967b17f4ae4f42bc4a0f900c7
manju16-17 <manju7654332@gmail.com>
2025-12-01 18:32:42 +0530

    Add ReturnDemo class to illustrate return behavior
    Demonstrate the use of return statement in Java.

---
+++ prog35.java
@@ -0,0 +1,9 @@
+public class ReturnDemo {
+    public static void main(String[] args) {
+        System.out.println("Before return");
+
+        if (true) return;
+
+        System.out.println("After return"); // will not execute
+    }
+}
