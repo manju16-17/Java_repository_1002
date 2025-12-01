6eaaadd252faf52da190ebcd9c3e94f8fd09d593
manju16-17 <manju7654332@gmail.com>
2025-12-01 18:16:00 +0530

    Implement modulus operation with user input

---
+++ prog10.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ModulusOperation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        if (num2 == 0) {
+            System.out.println("Error: Modulus by zero is not allowed.");
+        } else {
+            int result = num1 % num2;
+            System.out.println("Result of " + num1 + " % " + num2 + " = " + result);
+        }
+
+        sc.close();
